7828c52f479fd7da6845b69d145da7c4440ad90c
JerseyCalderwood56 <74026975+JerseyCalderwood56@users.noreply.github.com>
2021-04-16 11:06:55 -0700

    Updates
    Updates

---
+++ clean_data.py
@@ -0,0 +1,15 @@
+# data = [['1', '2020-12-30', 'kevin@example.com', 'mary@example.com', 'Demo', 'Now is the time...', 'mail_source1.csv'], ['2', '2020-12-30', 'kevin@example.com', 'mary@example.com', 'Demo', 'Now is the time...', 'mail_source1.csv'], ['3', '2020-12-29', 'bob@example.com', 'mary@example.com', 'Demo', 'Now is the time...', 'mail_source1.csv'], ['4', '2020-12-29', 'bob@example.com', 'mary@example.com', 'Demo', 'Now is the time...', 'mail_source1.csv'], ['5', '2020-12-29', 'info@example.com', 'mary@example.com', 'Demo', 'Now is the time...', 'mail_source1.csv'], ['1', '2020-12-30', 'kevin@example.com', 'joe@example.com', 'Demo', 'Now is the time...', 'mail_source2.csv'], ['2', '2020-12-30', 'kevin@example.com', 'joe@example.com', 'Demo', 'Now is the time...', 'mail_source2.csv'], ['3', '2020-12-28', 'bob@example.com', 'joe@example.com', 'Demo', 'Now is the time...', 'mail_source2.csv'], ['4', '2020-12-28', 'sales@example.com', 'joe@example.com', 'Demo', 'Now is the time...', 'mail_source2.csv'], ['5', '2020-12-29', 'bob@example.com', 'joe@example.com', 'Demo', 'Now is the time...', 'mail_source2.csv'], ['1', '2020-12-30', 'kevin@example.com', 'alice@example.com', 'Demo', 'Now is the time...', 'data'], ['2', '2020-12-30', 'kevin@example.com', 'alice@example.com', 'Demo', 'Now is the time...', 'data'], ['3', '2020-12-29', 'bob@example.com', 'alice@example.com', 'Demo', 'Now is the time...', 'data'], ['4', '2020-12-29', 'bob@example.com', 'alice@example.com', 'Demo', 'Now is the time...', 'data'], ['5', '2020-12-29', 'info@example.com', 'alice@example.com', 'Demo', 'Now is the time...', 'data']]
+#
+# clean_output = []
+#
+# for  row in data:
+#     clean_row = []
+#     for column in row:
+#         clean_row.append(column.strip())
+#     clean_output.append(clean_row)
+#
+# print(clean_output)
+#
+# for row_index in range(len(data)):
+#     for column_index in row:
+#         column.strip()
---
+++ compare_lists.py
@@ -0,0 +1,6 @@
+first = ["a", 123, [1, 2, 3]]
+second = ["a", 123, [1, 2, 3]]
+existing = [first]
+if second not in existing:
+    existing.append(second)
+# print(first == second)
